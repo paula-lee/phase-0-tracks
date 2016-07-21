5a385ff01dcc84b7fdab02949a9e662c0c79d096
Paula Lee <pdlee877@gmail.com>
2016-07-21 00:13:46 -0500

    create hash with client information and input

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,13 +1,13 @@
 #pseudocode
-#create an empty hash
 #prompt designer/user for client information
-#add to the hash:
+#create hash by inputting the key values
 	#client's name (string)
 	#age (integer)
 	#number of children (integer)
 	#decor theme (string)
 	#has pets (boolean)
 	#wants plants (boolean)
+#then put the input values from the designer/user with key values
 #convert input to string, boolean, or integer
 #print out the hash
 #ask the user if there is anything they would like to change (no loop)
@@ -15,4 +15,37 @@
 	#else if user types the key name, prompt for a new value
 	#update the user input from string to symbol then update the key to the new value
 #print latest version of hash
+
+
+puts "What is the client's name?"
+name = gets.chomp
+
+puts "What is the client's age?"
+age = gets.chomp.to_i
+
+puts "How many children does the client have?"
+num_of_children = gets.chomp.to_i
+
+puts "Type of decor theme the client wants?"
+decor_theme = gets.chomp
+
+puts "Does the client have pets? (y/n)"
+pets = gets.chomp.downcase
+has_pets = (pets == "y" || pets == "yes")
+
+puts "Does the client want plants? (y/n)"
+plants = gets.chomp.downcase
+wants_plants = (plants == "y" || plants == "yes")
+
+client_details = {
+	"name" => name,
+	age: age,
+	num_of_children: num_of_children,
+	decor_theme: decor_theme,
+	has_pets: has_pets,
+	wants_plants: wants_plants
+}
+
+p client_details
+
 
