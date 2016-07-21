ad1e2ce64a98d3aeeabdf283c10fa4ec291f7a53
Paula Lee <pdlee877@gmail.com>
2016-07-20 23:47:44 -0500

    add pseudocode forrelease 3

---
+++ ruby/hashes.rb
@@ -0,0 +1,18 @@
+#pseudocode
+#create an empty hash
+#prompt designer/user for client information
+#add to the hash:
+	#client's name (string)
+	#age (integer)
+	#number of children (integer)
+	#decor theme (string)
+	#has pets (boolean)
+	#wants plants (boolean)
+#convert input to string, boolean, or integer
+#print out the hash
+#ask the user if there is anything they would like to change (no loop)
+	#if none, skip
+	#else if user types the key name, prompt for a new value
+	#update the user input from string to symbol then update the key to the new value
+#print latest version of hash
+
