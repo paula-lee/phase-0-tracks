7092c9ad3382c80b168df3a18bc41fdef67e0d3d
Paula Lee <pdlee877@gmail.com>
2016-07-23 01:13:49 -0500

    edit pseudocode, made name into an array

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,13 +1,13 @@
 # pseudocode
 # create a method called encrypt_name that can take first and last name
-# take the first and last name and make into an array
-	# separate the first and last name by splitting it so that first name is a value
+# take the first and last name and make into an array called name_array
+	# separate the first and last name by splitting it with a space so that first name is a value
 	# and last name is a value
 	# assign the split full name into a variable name called split_name
 # call .reverse on the split_name array so that it will swap the first name and the last name 
 	# originally thought of doing array[0], array[1] = array[1], array[0] to swap but found 
 	# .reverse is shorter and cleaner
-# create a new variable called swapped_name and assign it to the new swapped name
+# create a new variable called swapped_name and assign it to the new swapped name array
 # create a method called vowel
 	# define the vowels 
 	# if there is a space " ", leave it alone
@@ -18,8 +18,12 @@
 	# if there is a space " ", leave it alone
 	# if the full_name includes consonants, then return the next consonant letter
 	# if the vowel is "z", make it an "b"
-	
+
+#commit message changes => edit pseudocode
 
 
 def encrypt_name(full_name)
+	name_array = full_name.split(" ")
 end
+
+encrypt_name("Paula Lee")
