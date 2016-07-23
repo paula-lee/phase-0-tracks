60968052a34d3de56b02db75e0e148968be40674
Paula Lee <pdlee877@gmail.com>
2016-07-23 01:10:32 -0500

    add pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,25 @@
+# pseudocode
+# create a method called encrypt_name that can take first and last name
+# take the first and last name and make into an array
+	# separate the first and last name by splitting it so that first name is a value
+	# and last name is a value
+	# assign the split full name into a variable name called split_name
+# call .reverse on the split_name array so that it will swap the first name and the last name 
+	# originally thought of doing array[0], array[1] = array[1], array[0] to swap but found 
+	# .reverse is shorter and cleaner
+# create a new variable called swapped_name and assign it to the new swapped name
+# create a method called vowel
+	# define the vowels 
+	# if there is a space " ", leave it alone
+	# if full_name includes vowels, then return the next vowel letter
+	# if the vowel is "u", make it an "a"
+# create a method called consonant
+	# define the consonants
+	# if there is a space " ", leave it alone
+	# if the full_name includes consonants, then return the next consonant letter
+	# if the vowel is "z", make it an "b"
+	
+
+
+def encrypt_name(full_name)
+end
