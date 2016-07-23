8a01949bf7a2b4a48da1d4c42f4389c8b081937f
Paula Lee <pdlee877@gmail.com>
2016-07-23 11:39:50 -0500

    add vowels method and tested the edge case

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,11 +3,13 @@
 # take the first and last name and make into an array called name_array
 	# separate the first and last name by splitting it with a space so that first name is a value
 	# and last name is a value
-	# assign the split full name into a variable name called split_name
 # call .reverse on the split_name array so that it will swap the first name and the last name 
 	# originally thought of doing array[0], array[1] = array[1], array[0] to swap but found 
 	# .reverse is shorter and cleaner
+	# then join the swapped values together so that last name and first name are no longer
+	# separate values
 # create a new variable called swapped_name and assign it to the new swapped name array
+# create a new variable called name_characters and split the swapped_name array into each characters
 # create a method called vowel
 	# define the vowels 
 	# if there is a space " ", leave it alone
@@ -24,6 +26,23 @@
 
 def encrypt_name(full_name)
 	name_array = full_name.split(" ")
+	swapped_name = name_array.reverse.join(" ")
+	name_characters = swapped_name.split("")
+	vowels(name_characters)
 end
 
-encrypt_name("Paula Lee")
+def vowels(characters)
+	vowels = "aeiou"
+	new_char = ""
+		characters.map! do |char|
+			if char == " "
+				" "
+			end
+			if char == "u"
+				new_char = "a"
+			end
+		end
+		p characters.join(" ")
+end
+
+encrypt_name("paula lee")
