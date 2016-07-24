a73a04e0576e9bbb2f899526a84d96c4b30e6a1c
Paula Lee <pdlee877@gmail.com>
2016-07-24 10:45:58 -0500

    implementing bubble sort method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -20,74 +20,85 @@
 # # => nil
 
 #release 1
-def fib(num)
-  first = 0
-  second = 1
-  sequence = []
+# def fib(num)
+#   first = 0
+#   second = 1
+#   sequence = []
   
-  num.times do
-    keep_first = first
-    first = second
-    second = keep_first + second
-    sequence << keep_first
-  end
+#   num.times do
+#     keep_first = first
+#     first = second
+#     second = keep_first + second
+#     sequence << keep_first
+#   end
 
-  return sequence
-end
+#   return sequence
+# end
 
-puts fib(6).inspect
-puts fib(100).inspect
+# puts fib(6).inspect
+# puts fib(100).inspect
 
-# #verify that fib(100) is == to 218922995834555169026
+# # #verify that fib(100) is == to 218922995834555169026
 
-if fib(100).include?(218922995834555169026)
-	puts true
-else
-	puts false
-end
+# if fib(100).include?(218922995834555169026)
+# 	puts true
+# else
+# 	puts false
+# end
 
 =begin
 release 3
 1. Researching the bubble sort
-2. Found a general bubble_sort template and just wrote down what each line does
-   ran it to make sure that what I said is correct and then print certain lines 
-   just to get a closer look at what it's actually doing
+2. Paula - Found a general bubble_sort template and ran each line separately
+   just to get a closer look at what it's actually doing. I definitely need to see
+   it in order to understand what is going on, especially being a visual person.
 3. Paula - I personally feel quite curious as I felt that to swap array values
     to another array values would be much simpler, also when I first saw the method 
     I thought it looked more complicated but after talking it out, it makes a lot of
     sense as to what it is doing. I should probably write out what the method is doing
     so that I understand what's going on much easier and if there is a part I don't 
     understand it would be easier to look up. 
-4. 
-
+4. pseudocode the bubble sort method
+    #bubble_sort method has one parameter that takes an array
+      #define a variable and make it equal to the length of the array
+      #create a loop 
+          #define the variable sort and make it equal to the boolean value of false
+          #length of the array minus 1 and will loop that many times
+          #if the array value is less than the array value plus 1
+          #then array value, and array value plus 1, 
+          #now equal to array value plus one and array value (swapped respectively)
+          #change the sort boolean value to true
+          #if the array values did not swap then stop the loop
+          #return the array
 =end
 
-
-#bubble_sort method has one parameter that takes an array
 def bubble_sort(array)
-  #define a variable by getting the length of the array
-  n = array.length
-  #starting a loop
+  array_length = array.length
   loop do
-    #defining swapped to the boolean value of false
-    swapped = false
+    sort = false
 
-    #length of the array minus 1, looping using the times method
-    (n-1).times do |i|
-      #if the array value is less than the array value plus 1
-      if array[i] > array[i+1]
-        #then array value, and array value plus 1, now equal to array value plus one and array value (swapped respectively)
-        array[i], array[i+1] = array[i+1], array[i]
-        #changes the swapped boolean value to true
-        swapped = true
+    (array_length -1).times do |num|
+      if array[num] > array[num+1]
+        array[num], array[num+1] = array[num+1], array[num]
+        sort = true
       end
     end
-    #if the array values do not swap then break the loop
-    break if not swapped
+
+    break if not sort
   end
-  #implicit return the array
+
   array
 end
+
+
+random_numbers = [9, 3, 7, 1, 6, 4, 8]
+
+puts bubble_sort(random_numbers)
+
+
+
+
+
 
 
 
