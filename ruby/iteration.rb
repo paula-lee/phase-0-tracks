b55994192feff76278dbd8d77169e29c9af9d168
Paula Lee <pdlee877@gmail.com>
2016-07-24 10:50:28 -0500

    add comments

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -30,17 +30,29 @@ puts weather
 
 
 # Release 2
+#array version
 sample_array = [1, 2, 3, 4, 5, 6]
+
 sample_array.delete_if {|number| number < 5}
+
 sample_array.keep_if {|number| number < 5}
+
 sample_array.select {|number| number.even?}
+
 sample_array.reject {|number| number.odd?}
+
 sample_array.take_while {|number| number < 3}
 
 
+#hash version
 sample_hash = {penny: 1, nickel: 5, dime: 10, quarter: 25}
+
 sample_hash.delete_if {|coin, value| value < 5}
+
 sample_hash.keep_if {|coin, value| value > 5}
+
 sample_hash.select {|coin, value| value > 5}
+
 sample_hash.reject {|coin, value| value < 5}
+
 sample_hash.keep_if {|coin, value| value > 5}
