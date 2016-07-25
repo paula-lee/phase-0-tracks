22f3c63e311d4f2299ec9ed70ed89ae12023c32f
Paula Lee <pdlee877@gmail.com>
2016-07-25 15:19:15 -0500

    add puppy design comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# release 0
+
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Breed:
+# Weight:
+# Friendly: 
+# Name: 
+# Coat Color:
+
+# BEHAVIOR -----------------------
+# Bark
+# Playful
+# Cuddler
+
+
