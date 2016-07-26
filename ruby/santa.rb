a1ea5d95278e128aa303924b7fbc78c0dd6a47e6
Paula Lee <pdlee877@gmail.com>
2016-07-26 12:42:37 -0500

    fix release 1: reindeer_ranking and age inside initialize method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,12 @@
 class Santa
 
-	def initialize
-		puts "Initializing Santa instance..."
+	def initialize(gender, ethnicity, santa_name)
+		@gender = gender
+		@ethnicity = ethnicity
+		@santa_name = santa_name
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+		# puts "Initializing Santa instance..."
 	end
 
 	def speak
@@ -14,7 +19,23 @@ class Santa
 
 end
 
+# santa_info = []
 
-santa = Santa.new
-santa.speak
-santa.eat_milk_and_cookies("white chocolate chip")
+# genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# santa_name = ["Santa", "Santa Claus", "Kris", "Kris Kringle", "Pere Noel", "Saint Nick", "Nick"]
+
+# puts "Making santas"
+# genders.length.times do |num|
+# 	santa_info << Santa.new(genders[num], ethnicities[num], santa_name[num])
+# end
+
+p santa_info
+
+
+
+# santa = Santa.new("girl", "asian")
+# santa.speak
+# santa.eat_milk_and_cookies("white chocolate chip")
+# santa.reindeer_ranking
+# santa.age
