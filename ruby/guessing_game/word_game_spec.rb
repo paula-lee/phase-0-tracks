157b9ee93caf16cb02bc8e7f1cbd7ed993996422
Paula Lee <pdlee877@gmail.com>
2016-07-29 11:14:35 -0500

    created a class on ruby file and require relative on spec

---
+++ ruby/guessing_game/word_game_spec.rb
@@ -0,0 +1,15 @@
+#pokemon game should do....
+	# 	choose a random pokemon from the pokemon names instance variable 
+	# 	split it by character so that it becomes an array with each letter
+	# 	as a value. 
+	#   Have the split pokemon name chosen to equal to who’s that 			
+	# 	pokemon variable name
+
+
+	require_relative 'word_game'
+
+	describe Pokemon do
+		let(:game) { Pokemon.new }
+
+		it
+	end
