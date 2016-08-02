95fc8e9f6f4abaf648ca214f23839f1fdba9dd36
Paula Lee <pdlee877@gmail.com>
2016-08-02 01:04:11 -0500

    create function reverse, create for loop, add driver code

--- js/explore.js
+++ js/explore.js
@@ -1,20 +1,24 @@
-// create a variable called reverse word and have it equal to an empty string
 // create a function called reverse that has 
 // parameter of word
+	// create a variable called reverse word and have it equal to an empty string
 	// the word will be passed through the function and the loop will
 	// take the last letter of the word and input into the
 	// reverse word variable
 	// print out the reversed word
 
 
-var reverse_word = ''
-
 function reverse(word) {
-	// reverses word
+	var reverseWord = '';
+	for (var i = word.length - 1; i >= 0; i--) {
+		reverseWord += word[i];
+	}
+	return (reverseWord);
 }
 
 
-
-
-
 // driver code
+var result = reverse("JavaScript");
+console.log(result);
+
+result = reverse("dangerous")
+console.log(result);
