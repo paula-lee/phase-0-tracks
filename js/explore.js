cd9ab7615ee545e43010ad907a66fd4c51870012
Paula Lee <pdlee877@gmail.com>
2016-08-02 00:20:43 -0500

    finish pseudocode

--- js/explore.js
+++ js/explore.js
@@ -1,6 +1,10 @@
+// create a variable called reverse word and have it equal to an empty string
 // create a function called reverse that has 
 // parameter of word
-// 
+	// the word will be passed through the function and the loop will
+	// take the last letter of the word and input into the
+	// reverse word variable
+	// print out the reversed word
 
 
 var reverse_word = ''
