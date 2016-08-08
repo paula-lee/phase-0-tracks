19d849e734da0d9325f71f072cc7f827fc4c843a
Paula Lee <pdlee877@gmail.com>
2016-08-07 22:07:20 -0500

    put in pseudocode for release 0

--- js/algos.js
+++ js/algos.js
@@ -1,20 +1,39 @@
-function longestWord(array) {
-	largest = 0; 
-	word = "";
 
-	for (var i = 0; i < array.length; i++) {
-		if (array[i].length > largest) {
-			largest = array[i].length;
-			word = array[i];
-		}
-	}
-	console.log(word);
+// Release 1: Find a Key-Value Match
+function matchObjects(object1, object2) {
+
 }
 
 
-// Driver code for release 0
-var phrase = ["long phrase", "longest phrase", "longer phrase"];
-longestWord(phrase);
+// Release 0: Find the Longest Phrase function
+// create a function called longestWord
+// the function will have a parameter that takes in an array
+// set two variables: one will be named 'largest' set to equal 0 
+// 					  two will be named 'word' set equal to ""
+// these two variables will be used to check if the word is the longest word
+// write a for loop 
+	// loop until the length of the array
+	// if the array length is greater than 0 or the previous word
+		// then that word will be equal to the variable largest
+		// then if it is the largest word it will be assigned to the variable word
+		
+// function longestWord(array) {
+// 	largest = 0; 
+// 	word = "";
 
-var words = ["light", "hippocampus", "dangerously", "two", "incredible"];
-longestWord(words);
+// 	for (var i = 0; i < array.length; i++) {
+// 		if (array[i].length > largest) {
+// 			largest = array[i].length;
+// 			word = array[i];
+// 		}
+// 	}
+// 	console.log(word);
+// }
+
+
+// Driver code for release 0
+// var phrase = ["long phrase", "longest phrase", "longer phrase"];
+// longestWord(phrase);
+
+// var words = ["light", "hippocampus", "dangerously", "two", "incredible"];
+// longestWord(words);
