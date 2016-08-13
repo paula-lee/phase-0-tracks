ec8807fd92a649d07e26604d976b2ae223b17418
Paula Lee <pdlee877@gmail.com>
2016-08-13 15:47:27 -0500

    add parts and orders table

--- customer_orders/customer_orders.rb
+++ customer_orders/customer_orders.rb
@@ -17,3 +17,26 @@ create_table_customers = <<-SQL
 SQL
 db.execute(create_table_customers)
 
+create_table_parts = <<-SQL
+	CREATE TABLE IF NOT EXISTS parts(
+		partsId INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		description VARCHAR(255),
+		quantity INT,
+		price DECIMAL(20, 2)
+	)
+SQL
+db.execute(create_table_parts)
+
+create_table_orders = <<-SQL
+	CREATE TABLE IF NOT EXISTS orders(
+		ordersId INTEGER PRIMARY KEY,
+		partsId INT,
+		quantity INT,
+		customerId INT,
+		FOREIGN KEY (partsId) REFERENCES parts(partsId),
+		FOREIGN KEY (customerId) REFERENCES customer(customerId)
+	)
+SQL
+
+db.execute(create_table_orders)
