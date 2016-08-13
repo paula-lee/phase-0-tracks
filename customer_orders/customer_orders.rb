ef656bd774d012d7f75ebe5e52243e73112715db
Paula Lee <pdlee877@gmail.com>
2016-08-13 15:35:56 -0500

    add customers table

--- customer_orders/customer_orders.rb
+++ customer_orders/customer_orders.rb
@@ -2,3 +2,18 @@ require 'sqlite3'
 require 'faker'
 
 db = SQLite3::Database.new("order_parts.db")
+
+#create table for customers
+
+create_table_customers = <<-SQL
+	CREATE TABLE IF NOT EXISTS customers(
+		customerId INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		company VARCHAR(255),
+		phoneNumber VARCHAR(20),
+		billingAddress VARCHAR(255),
+		shippingAddress VARCHAR(255)
+	)
+SQL
+db.execute(create_table_customers)
+
