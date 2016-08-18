eb8c999a5e6cf3accf9d6164bf480e2e9d04d8f4
Paula Lee <pdlee877@gmail.com>
2016-08-17 20:26:30 -0700

    add carlton image and add animation to javascript

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -4,3 +4,20 @@ var i;
 for (i=0; i < pText.length; i++) {
 	pText[i].style.border = "2px solid pink";
 }
+
+function dancing(event) {
+  var position = 0;
+  var id = setInterval(frame, 15);
+  function frame() {
+    if (position == 1920) {
+      clearInterval(id);
+    } else {
+      position++;
+      event.target.style.top = position + 'px';
+      event.target.style.left = position + 'px';
+    }
+  }
+}
+
+var carlton = document.getElementById("animate");
+carlton.addEventListener("click", dancing);
